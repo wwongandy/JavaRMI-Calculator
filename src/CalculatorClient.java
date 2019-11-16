4d2cd8842b128b7a39d6b97ada013a33164d2138
Andy <aw087ok@gmail.com>
2019-11-16 15:27:29 +0000

    Calculator history now supports multiple operations

--- src/CalculatorClient.java
+++ src/CalculatorClient.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JTextPane;
@@ -21,7 +22,7 @@ public class CalculatorClient {
 	
 	// Calculator management
 	private Calculator calculator;
-	private String[] calculatorHistory; // Most recently selected numbers and operations: [number, operation, number]
+	private ArrayList<String> calculatorHistory; // Most recently selected numbers and operations: [number, operation, number]
 	
 	/**
 	 * Launch the application.
@@ -70,6 +71,7 @@ public class CalculatorClient {
 		JButton buttonDivide = new JButton("/");
 		buttonDivide.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				operationPressed("/");
 			}
 		});
 		buttonDivide.setBounds(10, 90, 45, 40);
@@ -78,6 +80,7 @@ public class CalculatorClient {
 		JButton buttonMultiply = new JButton("*");
 		buttonMultiply.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				operationPressed("*");
 			}
 		});
 		buttonMultiply.setBounds(10, 130, 45, 40);
@@ -86,6 +89,7 @@ public class CalculatorClient {
 		JButton buttonMinus = new JButton("-");
 		buttonMinus.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				operationPressed("-");
 			}
 		});
 		buttonMinus.setBounds(10, 170, 45, 40);
@@ -94,6 +98,7 @@ public class CalculatorClient {
 		JButton buttonAdd = new JButton("+");
 		buttonAdd.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				operationPressed("+");
 			}
 		});
 		buttonAdd.setBounds(10, 210, 45, 40);
@@ -102,6 +107,7 @@ public class CalculatorClient {
 		JButton buttonSeven = new JButton("7");
 		buttonSeven.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				numberPressed("7");
 			}
 		});
 		buttonSeven.setBounds(55, 90, 45, 40);
@@ -110,6 +116,7 @@ public class CalculatorClient {
 		JButton buttonEight = new JButton("8");
 		buttonEight.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				numberPressed("8");
 			}
 		});
 		buttonEight.setBounds(100, 90, 45, 40);
@@ -118,6 +125,7 @@ public class CalculatorClient {
 		JButton buttonNine = new JButton("9");
 		buttonNine.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				numberPressed("9");
 			}
 		});
 		buttonNine.setBounds(145, 90, 45, 40);
@@ -126,6 +134,7 @@ public class CalculatorClient {
 		JButton buttonFour = new JButton("4");
 		buttonFour.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				numberPressed("4");
 			}
 		});
 		buttonFour.setBounds(55, 130, 45, 40);
@@ -134,6 +143,7 @@ public class CalculatorClient {
 		JButton buttonFive = new JButton("5");
 		buttonFive.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				numberPressed("5");
 			}
 		});
 		buttonFive.setBounds(100, 130, 45, 40);
@@ -142,6 +152,7 @@ public class CalculatorClient {
 		JButton buttonSix = new JButton("6");
 		buttonSix.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				numberPressed("6");
 			}
 		});
 		buttonSix.setBounds(145, 130, 45, 40);
@@ -150,6 +161,7 @@ public class CalculatorClient {
 		JButton buttonOne = new JButton("1");
 		buttonOne.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				numberPressed("1");
 			}
 		});
 		buttonOne.setBounds(55, 170, 45, 40);
@@ -158,6 +170,7 @@ public class CalculatorClient {
 		JButton buttonTwo = new JButton("2");
 		buttonTwo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				numberPressed("2");
 			}
 		});
 		buttonTwo.setBounds(100, 170, 45, 40);
@@ -166,6 +179,7 @@ public class CalculatorClient {
 		JButton buttonThree = new JButton("3");
 		buttonThree.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				numberPressed("3");
 			}
 		});
 		buttonThree.setBounds(145, 170, 45, 40);
@@ -174,6 +188,7 @@ public class CalculatorClient {
 		JButton buttonZero = new JButton("0");
 		buttonZero.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				numberPressed("0");
 			}
 		});
 		buttonZero.setBounds(55, 210, 45, 40);
@@ -207,46 +222,14 @@ public class CalculatorClient {
 	 * Performs the calculation process using the RMI instance's calculator.
 	 */
 	private void doCalculation() {
-		if (calculatorHistory.length != 3) {
-			return;
-		};
 		
-		float num1 = Float.parseFloat(calculatorHistory[0]);
-		float num2 = Float.parseFloat(calculatorHistory[2]);
-		
-		try {
-			float result = 0;
-			
-			switch (calculatorHistory[1]) {
-				case "+":
-					result = calculator.add(num1, num2);
-					break;
-					
-				case "-":
-					result = calculator.minus(num1, num2);
-					break;
-					
-				case "*":
-					result = calculator.multiply(num1, num2);
-					break;
-					
-				case "/":
-					result = calculator.divide(num1, num2);
-					break;
-			}
-			
-			System.out.println("Result calculated: " + result);
-		} catch (RemoteException e) {
-			System.out.println("Client error " + e);
-			// e.printStackTrace("Client error: " + e);
-		}
 	}
 	
 	/**
 	 * Resets the calculator history.
 	 */
-	private void clearCalculatorHistory() { 
-		calculatorHistory = new String[3];
+	private void clearCalculatorHistory() {
+		calculatorHistory = new ArrayList<String>();
 	}
 	
 	/**
@@ -256,15 +239,21 @@ public class CalculatorClient {
 	 * @param number
 	 */
 	private void numberPressed(String number) {
-		switch(calculatorHistory.length) {
-			case 3:
-				calculatorHistory[2] += number.toString();
-				break;
-				
-			default:
-				calculatorHistory[0] += number.toString();
-				break;
-		}
+		
+		if (calculatorHistory.isEmpty()) {
+			calculatorHistory.add(number);
+		} else {
+			
+			int size = calculatorHistory.size();
+			int lastIndex = size - 1;
+			String recent = calculatorHistory.get(lastIndex);
+			
+			if (recent.equals("+") || recent.equals("-") || recent.equals("*") || recent.equals("/")) {
+				calculatorHistory.add(number);
+			} else {
+				calculatorHistory.set(lastIndex, recent + "" + number);
+			}
+		};
 	}
 	
 	/**
@@ -274,17 +263,17 @@ public class CalculatorClient {
 	 * @param operation
 	 */
 	private void operationPressed(String operation) {
-		switch(calculatorHistory.length) {
-			case 3:
-				break;
-		
-			case 1:
-				if (calculatorHistory[0].isEmpty()) {
-					break;
-				}
-		
-			default:
-				calculatorHistory[1] = operation;
-		}
+		if (calculatorHistory.isEmpty()) {
+			return;
+		} else {
+			
+			int size = calculatorHistory.size();
+			int lastIndex = size - 1;
+			String recent = calculatorHistory.get(lastIndex);
+			
+			if (!(recent.equals("+") || recent.equals("-") || recent.equals("*") || recent.equals("/"))) {
+				calculatorHistory.add(operation);
+			}
+		};
 	}
 }
